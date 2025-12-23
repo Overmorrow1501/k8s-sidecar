4cf0ca348193932213d4749cfcaf5356359de248
molecule <molecule@gitlab.aruba.it>
2025-12-23 18:45:15 +0100

    make auth conditional

--- src/helpers.py
+++ src/helpers.py
@@ -1,35 +1,58 @@
 #!/usr/bin/env python
 
+import argparse
 import errno
 import hashlib
 import os
 import stat
 import subprocess
 from datetime import datetime
+from types import SimpleNamespace
 
 import requests
 from requests.adapters import HTTPAdapter
 from requests.auth import HTTPBasicAuth
 from requests.packages.urllib3.util.retry import Retry
-from types import SimpleNamespace
 
 from logger import get_logger
-import argparse
 
 parser = argparse.ArgumentParser(description="CLI flags for kiwigrid sidecar")
-parser.add_argument("--req-username-file", type=str, metavar=argparse.REMAINDER, help="path to file containing basic-auth username for REQ. This takes precedence over the environment variable REQ_USERNAME")
-parser.add_argument("--req-password-file", type=str, metavar=argparse.REMAINDER, help="path to file containing basic-auth password for REQ. This takes precedence over the environment variable REQ_PASSWORD")
+parser.add_argument(
+    "--req-username-file",
+    type=str,
+    metavar=argparse.REMAINDER,
+    help="path to file containing basic-auth username for REQ. This takes precedence over the environment variable REQ_USERNAME",
+)
+parser.add_argument(
+    "--req-password-file",
+    type=str,
+    metavar=argparse.REMAINDER,
+    help="path to file containing basic-auth password for REQ. This takes precedence over the environment variable REQ_PASSWORD",
+)
 args = parser.parse_args()
 
-CONTENT_TYPE_TEXT          = "ascii"
+CONTENT_TYPE_TEXT = "ascii"
 CONTENT_TYPE_BASE64_BINARY = "binary"
 
-REQ_RETRY_TOTAL          = 5 if os.getenv("REQ_RETRY_TOTAL") is None else int(os.getenv("REQ_RETRY_TOTAL"))
-REQ_RETRY_CONNECT        = 10 if os.getenv("REQ_RETRY_CONNECT") is None else int(os.getenv("REQ_RETRY_CONNECT"))
-REQ_RETRY_READ           = 5 if os.getenv("REQ_RETRY_READ") is None else int(os.getenv("REQ_RETRY_READ"))
-REQ_RETRY_BACKOFF_FACTOR = 1.1 if os.getenv("REQ_RETRY_BACKOFF_FACTOR") is None else float(
-    os.getenv("REQ_RETRY_BACKOFF_FACTOR"))
-REQ_TIMEOUT              = 10 if os.getenv("REQ_TIMEOUT") is None else float(os.getenv("REQ_TIMEOUT"))
+REQ_RETRY_TOTAL = (
+    5 if os.getenv("REQ_RETRY_TOTAL") is None else int(os.getenv("REQ_RETRY_TOTAL"))
+)
+REQ_RETRY_CONNECT = (
+    10
+    if os.getenv("REQ_RETRY_CONNECT") is None
+    else int(os.getenv("REQ_RETRY_CONNECT"))
+)
+REQ_RETRY_READ = (
+    5 if os.getenv("REQ_RETRY_READ") is None else int(os.getenv("REQ_RETRY_READ"))
+)
+REQ_RETRY_BACKOFF_FACTOR = (
+    1.1
+    if os.getenv("REQ_RETRY_BACKOFF_FACTOR") is None
+    else float(os.getenv("REQ_RETRY_BACKOFF_FACTOR"))
+)
+REQ_TIMEOUT = (
+    10 if os.getenv("REQ_TIMEOUT") is None else float(os.getenv("REQ_TIMEOUT"))
+)
 
 # Allows to suppress TLS verification for all HTTPs requests (except to the API server, which are controller by SKIP_TLS_VERIFY)
 # This is particularly useful when the connection to the main container happens as "localhost"
@@ -70,8 +93,10 @@ def write_data_to_file(folder, filename, data, data_type=CONTENT_TYPE_TEXT):
             if e.errno not in (errno.EACCES, errno.EEXIST):
                 raise
             if e.errno == errno.EACCES:
-                logger.error(f"Error: insufficient privileges to create {folder}. "
-                             f"Skipping {filename}.")
+                logger.error(
+                    f"Error: insufficient privileges to create {folder}. "
+                    f"Skipping {filename}."
+                )
                 return
 
     absolute_path = os.path.join(folder, filename)
@@ -80,15 +105,17 @@ def write_data_to_file(folder, filename, data, data_type=CONTENT_TYPE_TEXT):
         if data_type == "binary":
             sha256_hash_new = hashlib.sha256(data)
         else:
-            sha256_hash_new = hashlib.sha256(data.encode('utf-8'))
+            sha256_hash_new = hashlib.sha256(data.encode("utf-8"))
 
-        with open(absolute_path, 'rb') as f:
+        with open(absolute_path, "rb") as f:
             sha256_hash_cur = hashlib.sha256()
             for byte_block in iter(lambda: f.read(4096), b""):
                 sha256_hash_cur.update(byte_block)
 
         if sha256_hash_new.hexdigest() == sha256_hash_cur.hexdigest():
-            logger.debug(f"Contents of {filename} haven't changed. Not overwriting existing file")
+            logger.debug(
+                f"Contents of {filename} haven't changed. Not overwriting existing file"
+            )
             return False
 
     if data_type == "binary":
@@ -100,8 +127,8 @@ def write_data_to_file(folder, filename, data, data_type=CONTENT_TYPE_TEXT):
     with open(absolute_path, write_type) as f:
         f.write(data)
         f.close()
-    if os.getenv('DEFAULT_FILE_MODE'):
-        mode = int(os.getenv('DEFAULT_FILE_MODE'), base=8)
+    if os.getenv("DEFAULT_FILE_MODE"):
+        mode = int(os.getenv("DEFAULT_FILE_MODE"), base=8)
         os.chmod(absolute_path, mode)
     return True
 
@@ -119,7 +146,7 @@ def remove_file(folder, filename):
 
 def read_file_content(file_path):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             return file.read().strip()
     except FileNotFoundError:
         logger.warning(f"File not found at: {file_path}")
@@ -131,23 +158,30 @@ def read_file_content(file_path):
 
 
 def fetch_basic_auth_credentials():
-    username = os.getenv("REQ_USERNAME")
-    password = os.getenv("REQ_PASSWORD")
-    if args.req_username_file:
-        username_from_file = read_file_content(args.req_username_file)
-        if username_from_file is not None:
-            username = username_from_file
-    if args.req_password_file:
-        password_from_file = read_file_content(args.req_password_file)
-        if password_from_file is not None:
-            password = password_from_file
+    needed_auth = os.getenv("REQ_BASIC_AUTH", default=True)
+    username = password = None
+    if needed_auth:
+        username = os.getenv("REQ_USERNAME")
+        password = os.getenv("REQ_PASSWORD")
+        if args.req_username_file:
+            username_from_file = read_file_content(args.req_username_file)
+            if username_from_file is not None:
+                username = username_from_file
+        if args.req_password_file:
+            password_from_file = read_file_content(args.req_password_file)
+            if password_from_file is not None:
+                password = password_from_file
     return username, password
 
 
 def request(url, method, enable_5xx=False, payload=None):
     enforce_status_codes = list() if enable_5xx else [500, 502, 503, 504]
-    username,password = fetch_basic_auth_credentials()
-    encoding = 'latin1' if not os.getenv("REQ_BASIC_AUTH_ENCODING") else os.getenv("REQ_BASIC_AUTH_ENCODING")
+    username, password = fetch_basic_auth_credentials()
+    encoding = (
+        "latin1"
+        if not os.getenv("REQ_BASIC_AUTH_ENCODING")
+        else os.getenv("REQ_BASIC_AUTH_ENCODING")
+    )
     if username and password:
         auth = HTTPBasicAuth(username.encode(encoding), password.encode(encoding))
     else:
@@ -155,12 +189,14 @@ def request(url, method, enable_5xx=False, payload=None):
 
     r = requests.Session()
 
-    retries = Retry(total=REQ_RETRY_TOTAL,
-                    connect=REQ_RETRY_CONNECT,
-                    read=REQ_RETRY_READ,
-                    backoff_factor=REQ_RETRY_BACKOFF_FACTOR,
-                    allowed_methods=["GET", "POST"],
-                    status_forcelist=enforce_status_codes)
+    retries = Retry(
+        total=REQ_RETRY_TOTAL,
+        connect=REQ_RETRY_CONNECT,
+        read=REQ_RETRY_READ,
+        backoff_factor=REQ_RETRY_BACKOFF_FACTOR,
+        allowed_methods=["GET", "POST"],
+        status_forcelist=enforce_status_codes,
+    )
     r.mount("http://", HTTPAdapter(max_retries=retries))
     r.mount("https://", HTTPAdapter(max_retries=retries))
     if url is None:
@@ -170,19 +206,33 @@ def request(url, method, enable_5xx=False, payload=None):
     try:
         # If method is not provided use GET as default
         if method == "GET" or not method:
-            res = r.get("%s" % url, auth=auth, timeout=REQ_TIMEOUT, verify=REQ_TLS_VERIFY)  # nosem-py/request-without-cert-validation
-            logger.info(f"Request sent to {url}. Response: {res.status_code} {res.reason} {res.text}")
+            res = r.get(
+                "%s" % url, auth=auth, timeout=REQ_TIMEOUT, verify=REQ_TLS_VERIFY
+            )  # nosem-py/request-without-cert-validation
+            logger.info(
+                f"Request sent to {url}. Response: {res.status_code} {res.reason} {res.text}"
+            )
         elif method == "POST":
-            res = r.post("%s" % url, auth=auth, json=payload, timeout=REQ_TIMEOUT, verify=REQ_TLS_VERIFY)  # nosem-py/request-without-cert-validation
-            logger.info(f"{payload} sent to {url}. Response: {res.status_code} {res.reason} {res.text}")
+            res = r.post(
+                "%s" % url,
+                auth=auth,
+                json=payload,
+                timeout=REQ_TIMEOUT,
+                verify=REQ_TLS_VERIFY,
+            )  # nosem-py/request-without-cert-validation
+            logger.info(
+                f"{payload} sent to {url}. Response: {res.status_code} {res.reason} {res.text}"
+            )
         else:
-            logger.warning(f"Invalid REQ_METHOD: '{method}', please use 'GET' or 'POST'. Doing nothing.")
+            logger.warning(
+                f"Invalid REQ_METHOD: '{method}', please use 'GET' or 'POST'. Doing nothing."
+            )
             return
         return res
     except requests.exceptions.HTTPError as e:
         logger.error(f"HTTP error for URL {url}: {e}")
     except requests.exceptions.SSLError as e:
-        logger.error(f"SSL certificate verification failed for URL {url}: {e}")   
+        logger.error(f"SSL certificate verification failed for URL {url}: {e}")
     except requests.exceptions.RetryError as e:
         logger.error(f"Max retries exceeded for URL {url}: {e}")
     except Exception as e:
@@ -190,6 +240,7 @@ def request(url, method, enable_5xx=False, payload=None):
     # Return a dummy-object with empty attributes to avoid AttributeError if no response is returned (e.g. MaxRetryError)
     logger.debug(f"Returning dummy response for URL {url}")
     return SimpleNamespace(text="", content=b"")
+
 
 def timestamp():
     """Get a timestamp of the current time for logging."""
@@ -209,20 +260,20 @@ def unique_filename(filename, namespace, resource, resource_name):
     resource -- the resource type, e.g. "configmap" or "secret".
     resource_name -- the name of the "configmap" or "secret" resource instance.
     """
-    return "namespace_" + namespace + "." + resource + "_" + resource_name + "." + filename
+    return (
+        "namespace_" + namespace + "." + resource + "_" + resource_name + "." + filename
+    )
 
 
 def execute(script_path):
     logger.info(f"Executing script from {script_path}")
     try:
         if os.access(script_path, os.X_OK):
-            result = subprocess.run([script_path],
-                                    capture_output=True,
-                                    check=True)
+            result = subprocess.run([script_path], capture_output=True, check=True)
         else:
-            result = subprocess.run(["sh", script_path],
-                                    capture_output=True,
-                                    check=True)
+            result = subprocess.run(
+                ["sh", script_path], capture_output=True, check=True
+            )
         logger.debug(f"Script stdout: {result.stdout}")
         logger.debug(f"Script stderr: {result.stderr}")
         logger.debug(f"Script exit code: {result.returncode}")
